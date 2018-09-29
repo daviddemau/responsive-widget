178b217214d0e598f9f6ad3dad5e2be1502c04e1
daviddemau <35766740+daviddemau@users.noreply.github.com>
2018-09-29 22:26:29 +0200

    added user location API call

--- TaboolaTest.js
+++ TaboolaTest.js
@@ -1,11 +1,22 @@
 //Taboola API
 const url= 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init';
 
+const GetData = (yourUrl) => {
+    var Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
+
 //get application data
 const data = JSON.parse(GetData(url));
 const widgetsArray = data.list;
 
-console.log(widgetsArray)
+//get user location
+const locationData = JSON.parse(GetData('http://api.ipstack.com/91.164.43.8?access_key=964f49e09150dc34b415565677b4c4f2'));
+
+//change language for header based on user location
+changeHeaderLanguage(locationData.country_code)
 
 //select divs to display informations
 thumbnails = Array.from(document.querySelectorAll('.thumbnail'));
@@ -15,6 +26,7 @@ category = Array.from(document.querySelectorAll('#category'));
 
 //assign data from API to the corresponding widget element
 assignData();
+
 
 //functions
 function assignData(){
@@ -46,12 +58,6 @@ function sendToUrl(arr, destinationUrl) {
   })
 }
 
-function GetData(yourUrl){
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
-}
 
 function assignCategory(item, index) {
   if(item.categories !== undefined) {
@@ -86,11 +92,19 @@ function assignCategory(item, index) {
         category[index].style.color = 'green';
         break;
       case 'food':
-        category[index].style.color = 'purple';
+        category[index].style.color = 'rgb(131, 110, 72)';
         break;
-      case 'food':
+      case 'fashion':
         category[index].style.color = 'purple';
         break;
       }
+  }
+}
+
+function changeHeaderLanguage(location) {
+  if(location === 'US') {
+    document.querySelector('h2').innerHTML = 'You may be interested';
+  } else {
+    document.querySelector('h2').innerHTML = 'Du contenu pour vous';
   }
 }
