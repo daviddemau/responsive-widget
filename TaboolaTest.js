52b3b2f77e25040b53ba591672834049f938c331
daviddemau <35766740+daviddemau@users.noreply.github.com>
2018-09-29 12:28:54 +0200

    Add files via upload

--- TaboolaTest.js
+++ TaboolaTest.js
@@ -7,25 +7,32 @@ const widgetsArray = data.list;
 
 console.log(widgetsArray)
 
-//select divs where to show the informations
+//select divs to display informations
 thumbnails = Array.from(document.querySelectorAll('.thumbnail'));
 titles = Array.from(document.querySelectorAll('.title'));
 brand = Array.from(document.querySelectorAll('.branding'));
+category = Array.from(document.querySelectorAll('#category'));
 
 //assign data from API to the corresponding widget element
 assignData();
-
 
 //functions
 function assignData(){
   for(var i = 0; i < widgetsArray.length ; i++) {
     let item = widgetsArray[i];
+
     //assign thumbnail images
     thumbnails[i].style.backgroundImage = "url("+ item.thumbnail[0].url + ")";
+
     //assign item title
     titles[i].innerHTML = item.name;
+
     //assign branding
     brand[i].innerHTML = item.branding;
+
+    //assign category with correct css
+    assignCategory(item, i);
+
     //make all item elements clickable to redirect to corresponding url
     let arr = []
     arr.push(thumbnails[i], titles[i], brand[i]);
@@ -44,4 +51,46 @@ function GetData(yourUrl){
     Httpreq.open("GET",yourUrl,false);
     Httpreq.send(null);
     return Httpreq.responseText;
+}
+
+function assignCategory(item, index) {
+  if(item.categories !== undefined) {
+    category[index].innerHTML = item.categories[0];
+
+    switch (item.categories[0]) {
+      case 'education':
+        category[index].style.color = 'blue';
+        break;
+      case 'entertainment':
+        category[index].style.color = 'orange';
+        break;
+      case 'beauty':
+        category[index].style.color = 'pink';
+        break;
+      case 'tech':
+        category[index].style.color = 'cyan';
+        break;
+      case 'news':
+        category[index].style.color = 'red';
+        break;
+      case 'autos':
+        category[index].style.color = 'grey';
+        break;
+      case 'travel':
+        category[index].style.color = 'gold';
+        break;
+      case 'lifestyle':
+        category[index].style.color = 'orange';
+        break;
+      case 'health':
+        category[index].style.color = 'green';
+        break;
+      case 'food':
+        category[index].style.color = 'purple';
+        break;
+      case 'food':
+        category[index].style.color = 'purple';
+        break;
+      }
+  }
 }
