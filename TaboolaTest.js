f3873a64b7499482743095f3c7d70e78d8e5c87f
daviddemau <35766740+daviddemau@users.noreply.github.com>
2018-09-30 11:16:25 +0200

    asynchronous request created

--- TaboolaTest.js
+++ TaboolaTest.js
@@ -1,34 +1,24 @@
 //Taboola API
 const url= 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init';
 
-const GetData = (yourUrl) => {
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
-}
-
-//get application data
-const data = JSON.parse(GetData(url));
-const widgetsArray = data.list;
-
-//get user location
-const locationData = JSON.parse(GetData('http://api.ipstack.com/91.164.43.8?access_key=964f49e09150dc34b415565677b4c4f2'));
-
-//change language for header based on user location
-changeHeaderLanguage(locationData.country_code)
-
 //select divs to display informations
 thumbnails = Array.from(document.querySelectorAll('.thumbnail'));
 titles = Array.from(document.querySelectorAll('.title'));
 brand = Array.from(document.querySelectorAll('.branding'));
 category = Array.from(document.querySelectorAll('#category'));
 
+//get data from API
+getJSON(url, function(err, data) {
+  if (err !== null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    var response = data;
+  }
+  const widgetsArray = response.list;
+
 //assign data from API to the corresponding widget element
 assignData();
 
-
-//functions
 function assignData(){
   for(var i = 0; i < widgetsArray.length ; i++) {
     let item = widgetsArray[i];
@@ -57,7 +47,6 @@ function sendToUrl(arr, destinationUrl) {
     e.addEventListener('click', () => window.location = destinationUrl)
   })
 }
-
 
 function assignCategory(item, index) {
   if(item.categories !== undefined) {
@@ -101,10 +90,40 @@ function assignCategory(item, index) {
   }
 }
 
-function changeHeaderLanguage(location) {
-  if(location === 'US') {
-    document.querySelector('h2').innerHTML = 'You may be interested';
+});
+
+//get user location and change header language accordingly
+getJSON('http://api.ipstack.com/91.164.43.8?access_key=964f49e09150dc34b415565677b4c4f2', function(err, data) {
+  if (err !== null) {
+    alert('Something went wrong: ' + err);
   } else {
-    document.querySelector('h2').innerHTML = 'Du contenu pour vous';
+    var response = data;
   }
-}
+
+  const locationData = response;
+  //change language for header based on user location
+  changeHeaderLanguage(locationData.country_code)
+
+  function changeHeaderLanguage(country) {
+    if(country === "US") {
+      document.querySelector('h2').innerHTML = 'You may be interested';
+    } else {
+      document.querySelector('h2').innerHTML = 'Du contenu pour vous';
+    }
+  }
+})
+
+function getJSON(myUrl, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', myUrl, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
